d10f73afc45b3f7acf08718293a300504c5745ae
firstChairCoder <md1qq7@gmail.com>
2023-11-20 18:24:23 +0100

    chore: send file using absolute path

--- myApp.js
+++ myApp.js
@@ -3,10 +3,12 @@ let app = express();
 
 console.log("Hello World");
 
+let absPath = __dirname + '/views/index.html';
 
 
 app.get("/", (req, res) => {
-	res.send("Hello Express");
+	// res.send("Hello Express");
+	res.sendFile(absPath);
 });
 
 
