961bd6dff4285bc7e7f7a94bd513a688c4c8f7a8
firstChairCoder <md1qq7@gmail.com>
2023-11-20 22:58:26 +0100

    chore: create basic logger middleware

--- myApp.js
+++ myApp.js
@@ -8,6 +8,12 @@ let absPath = __dirname + '/views/index.html';
 let midPath = __dirname + '/public'
 
 
+
+app.use((req, res, next) => {
+	console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+	next();
+});
+
 app.get("/", (req, res) => {
 	// res.send("Hello Express");
 	res.sendFile(absPath);
