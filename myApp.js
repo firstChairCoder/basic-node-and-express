35648a1415eadf887d1082ea381e48ef65e652b0
firstChairCoder <md1qq7@gmail.com>
2023-11-21 12:19:01 +0100

    chore: add name GET request

--- myApp.js
+++ myApp.js
@@ -1,64 +1,50 @@
-require('dotenv').config();
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
 console.log("Hello World");
 
-let absPath = __dirname + '/views/index.html';
-let midPath = __dirname + '/public'
-
-
+let absPath = __dirname + "/views/index.html";
+let midPath = __dirname + "/public";
 
 app.use((req, res, next) => {
-	console.log(req.method + ' ' + req.path + ' - ' + req.ip);
-	next();
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
 });
 
 app.get("/", (req, res) => {
-	// res.send("Hello Express");
-	res.sendFile(absPath);
+  // res.send("Hello Express");
+  res.sendFile(absPath);
 });
-
-
 
 app.use("/public", express.static(midPath));
 
 app.get("/json", (req, res) => {
-	if(process.env.MESSAGE_STYLE === 'uppercase') {
-		res.json({"message": "HELLO JSON"})
-	} else {
-		res.json({"message": "Hello json"})
-	}
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
 
-app.get("/now", (req, res, next) => {
-	req.time = new Date().toString();
-	next();
-}, (req, res) => {
-	res.json({ "time": req.time})
-})
+app.get("/name", (req, res) => {
+  let fullName = req.query.first + " " + req.query.last;
+  res.json({ name: fullName });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
